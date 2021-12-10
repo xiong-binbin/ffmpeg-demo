17a26c59bb104f6b0a056b1b437c18e6cff87eb1
0769 <xiongbinbin@mudu.tv>
2021-12-10 14:23:31 +0800

    支持yuv混合

--- app/src/video_decode.cpp
+++ app/src/video_decode.cpp
@@ -85,13 +85,28 @@ void VideoDecode::decode(AVCodecContext *ctx, AVFrame *frame, AVPacket *pkt, con
     int i = 0;
     FILE *fp = NULL;
 
-    AVFrame* dstFrame = av_frame_alloc();
-    assert(NULL != dstFrame);
+    int x = 0;
+    int y = 0;
 
-    dstFrame->width = 640;
-    dstFrame->height = 480;
-    dstFrame->format = AV_PIX_FMT_YUV420P;
-    av_frame_get_buffer(dstFrame, 0);
+    AVFrame* bgFrame  = av_frame_alloc();
+    AVFrame* aFrame   = av_frame_alloc();
+    assert(NULL != bgFrame);
+    assert(NULL != aFrame);
+
+    bgFrame->width = 1280;
+    bgFrame->height = 720;
+    bgFrame->format = AV_PIX_FMT_YUV420P;
+    av_frame_get_buffer(bgFrame, 0);
+
+    //黑色背景
+    memset(bgFrame->data[0], 0xff, bgFrame->linesize[0]*bgFrame->height);
+    memset(bgFrame->data[1], 0x80, bgFrame->linesize[1]*bgFrame->height/2);
+    memset(bgFrame->data[2], 0x80, bgFrame->linesize[2]*bgFrame->height/2);
+
+    aFrame->width = 640;
+    aFrame->height = 480;
+    aFrame->format = AV_PIX_FMT_YUV420P;
+    av_frame_get_buffer(aFrame, 0);
 
     ret = avcodec_send_packet(ctx, pkt);
     assert(0 == ret);
@@ -109,18 +124,55 @@ void VideoDecode::decode(AVCodecContext *ctx, AVFrame *frame, AVPacket *pkt, con
         }
 
         libyuv::I420Scale(frame->data[0], frame->linesize[0], frame->data[1], frame->linesize[1], frame->data[2], frame->linesize[2], frame->width, frame->height, 
-            dstFrame->data[0], dstFrame->linesize[0], dstFrame->data[1], dstFrame->linesize[1], dstFrame->data[2], dstFrame->linesize[2], dstFrame->width, dstFrame->height, libyuv::kFilterBilinear);
+            aFrame->data[0], aFrame->linesize[0], aFrame->data[1], aFrame->linesize[1], aFrame->data[2], aFrame->linesize[2], aFrame->width, aFrame->height, libyuv::kFilterBilinear);
+
+        x = 0;
+        y = 0;
+
+        libyuv::I420Copy(aFrame->data[0] + aFrame->linesize[0],
+                         aFrame->linesize[0],
+                         aFrame->data[1] + aFrame->linesize[1],
+                         aFrame->linesize[1],
+                         aFrame->data[2] + aFrame->linesize[2],
+                         aFrame->linesize[2],
+                         bgFrame->data[0] + x + y*bgFrame->linesize[0],
+                         bgFrame->linesize[0],
+                         bgFrame->data[1] + x/2 + y/2*bgFrame->linesize[1],
+                         bgFrame->linesize[1],
+                         bgFrame->data[2] + x/2 + y/2*bgFrame->linesize[2],
+                         bgFrame->linesize[2],
+                         bgFrame->width - x >= aFrame->width ? aFrame->width : (bgFrame->width - x),
+                         bgFrame->height - y >= aFrame->height ? aFrame->height : (bgFrame->height - y));
+
+        x = 700;
+        y = 400;
+
+        libyuv::I420Copy(aFrame->data[0] + aFrame->linesize[0],
+                         aFrame->linesize[0],
+                         aFrame->data[1] + aFrame->linesize[1],
+                         aFrame->linesize[1],
+                         aFrame->data[2] + aFrame->linesize[2],
+                         aFrame->linesize[2],
+                         bgFrame->data[0] + x + y*bgFrame->linesize[0],
+                         bgFrame->linesize[0],
+                         bgFrame->data[1] + x/2 + y/2*bgFrame->linesize[1],
+                         bgFrame->linesize[1],
+                         bgFrame->data[2] + x/2 + y/2*bgFrame->linesize[2],
+                         bgFrame->linesize[2],
+                         bgFrame->width - x >= aFrame->width ? aFrame->width : (bgFrame->width - x),
+                         bgFrame->height - y >= aFrame->height ? aFrame->height : (bgFrame->height - y));
 
         snprintf(name, sizeof(name), "%s-%d", file, ctx->frame_number);
 
         fp = fopen(name, "wb");
-        fprintf(fp, "P5\n%d %d\n%d\n", dstFrame->width, dstFrame->height, 255);
-        for(i = 0; i < dstFrame->height; i++)
+        fprintf(fp, "P5\n%d %d\n%d\n", bgFrame->width, bgFrame->height, 255);
+        for(i = 0; i < bgFrame->height; i++)
         {
-            fwrite(dstFrame->data[0] + i * dstFrame->linesize[0], 1, dstFrame->width, fp);
+            fwrite(bgFrame->data[0] + i * bgFrame->linesize[0], 1, bgFrame->width, fp);
         }
         fclose(fp);
     }
 
-    av_frame_free(&dstFrame);
+    av_frame_free(&bgFrame);
+    av_frame_free(&aFrame);
 }
