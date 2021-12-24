fda21693903e70376ee6f0d271bbbcf431811160
0769 <xiongbinbin@mudu.tv>
2021-12-24 17:11:38 +0800

    update

---
+++ app/include/audio_filter.h
@@ -0,0 +1,29 @@
+/*
+ * @Author: your name
+ * @Date: 2021-12-17 16:15:23
+ * @LastEditTime: 2021-12-23 18:39:42
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /ffmpeg-demo/app/include/audio_filter.h
+ */
+#include "common.h"
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavfilter/buffersink.h>
+#include <libavfilter/buffersrc.h>
+#include <libavutil/opt.h>
+}
+
+class AudioFilter
+{
+public:
+    AudioFilter();
+    ~AudioFilter();
+
+private:
+    /* data */
+};
+
+
---
+++ app/src/audio_filter.cpp
@@ -0,0 +1,19 @@
+/*
+ * @Author: your name
+ * @Date: 2021-12-17 16:17:06
+ * @LastEditTime: 2021-12-17 16:19:01
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /ffmpeg-demo/app/src/audio_filter.cpp
+ */
+#include "audio_filter.h"
+
+AudioFilter::AudioFilter()
+{
+}
+
+AudioFilter::~AudioFilter()
+{
+}
+
+
